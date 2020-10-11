b6d354a98fe9519b116feec14a8ecf54775a1338
Arihant Tripathi <tarihant2001@gmail.com>
2020-10-10 22:55:02 -0400

    Format for Avatar

--- cogs/avatarcog.py
+++ cogs/avatarcog.py
@@ -6,19 +6,14 @@ class AvatarCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="avatar", aliases=["Avatar"])
+    async def avatar(self, ctx, user: discord.Member):
 
+        embed = discord.Embed(color=discord.Color(0xFFFF), title=f"{user}")
 
+        embed.set_image(url=f"{user.avatar_url}")
 
-    @commands.command(name = 'avatar', aliases = ['Avatar'])
-    async def avatar(self,ctx,user: discord.Member):
-
-	       embed = discord.Embed(color = discord.Color(0xffff) , title = f"{user}")
-
-	       embed.set_image(url=f"{user.avatar_url}")
-
-	       await ctx.send(embed=embed)
-
-
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
